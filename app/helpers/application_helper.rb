eb54383c3f3c7af5ae128f55bb1524ae8dbd9228
Dave Newton <davelnewton@gmail.com>
2011-06-26 18:06:19 -0400

    Crap; screwed up ch5-users branch commit. Also created 'logo' helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,8 @@ module ApplicationHelper
         return (@title.nil? || @title.strip.empty?) ? t : "#{t} | #{@title}"
     end
 
+    def logo
+        image_tag('logo.jpg', :alt => 'Sample App', :class => 'round')
+    end
+
 end
