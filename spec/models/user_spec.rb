86e2024e70aa55a3fa437b752cbdfc0437b13c82
Dave Newton <davelnewton@gmail.com>
2011-06-26 19:47:14 -0400

    Name and email validations, specs for same.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,61 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    before(:each) do
+        @attr = { :name => 'Example User', :email => 'wtf@over.com' }
+    end
+
+    it "should create a new instance given valid attributes" do
+        User.create!(@attr)
+    end
+
+    it "should require a name" do
+        failure = User.new(@attr.merge(:name => ''))
+        failure.should_not be_valid
+    end
+
+    it "should require an email" do
+        failure = User.new(@attr.merge(:email => ''))
+        failure.should_not be_valid
+    end
+
+    # TODO I'd like to keep the length in the User class--as a class var?
+    it "should reject names that are too long" do
+        failure = User.new(@attr.merge(:name => 'a'*51))
+        failure.should_not be_valid
+    end
+
+    it "should accept valid email addresses" do
+        valid_addrs = %w[user@foo.com WAT_WAT@nar.nar.nar first.last@long.subdomain]
+        valid_addrs.each do |addr|
+            user = User.new(@attr.merge({ :email => addr }))
+            user.should be_valid
+        end
+    end
+
+    it "should reject invalid email addresses" do
+        bad_addrs = %w[not_email.addr missing_at_sign.org no@subdomain.]
+        bad_addrs.each do |addr|
+            user = User.new(@attr.merge({ :email => addr }))
+            user.should_not be_valid
+        end
+    end
+
+    it "should reject duplicate email addresses" do
+        User.create!(@attr)
+        user = User.new(@attr)
+        user.should_not be_valid
+    end
+
+    it "should reject email addresses that differ only in upper/lower case" do
+        uc_email = @attr[:email].upcase
+
+        User.create!(@attr.merge({ :email => uc_email }))
+        user = User.new(@attr)
+        user.should_not be_valid
+    end
+
 end
 
 # == Schema Information
@@ -14,4 +68,3 @@ end
 #  created_at :datetime
 #  updated_at :datetime
 #
-
