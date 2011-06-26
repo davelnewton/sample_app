d4efc9f4f2b6fffc0cf938acc25dd2a2579cfe02
Dave Newton <davelnewton@gmail.com>
2011-06-26 12:33:51 -0400

    Moved 'pages' title tests into http success tests :/

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,37 +4,25 @@ describe PagesController do
     render_views
 
     describe "GET 'home'" do
-        it "should be successful" do
+        it "should be successful and have correct title" do
             get 'home'
             response.should be_success
-        end
-
-        it "should have the right title" do
-            get 'home'
-            response.should have_selector('title', :content => 'Ruby on Rails Tutorial Sample App | Home')
+            response.should have_selector('title', :content => ' | Home')
         end
     end
 
     describe "GET 'contact'" do
-        it "should be successful" do
+        it "should be successful and have correct title" do
             get 'contact'
             response.should be_success
-        end
-
-        it "should have the right title" do
-            get 'contact'
             response.should have_selector('title', :content => ' | Contact')
         end
     end
 
     describe "GET 'about'" do
-        it "should be successful" do
+        it "should be successful and have correct title" do
             get 'about'
             response.should be_success
-        end
-
-        it "should have the right title" do
-            get 'about'
             response.should have_selector('title', :content => ' | About')
         end
     end
