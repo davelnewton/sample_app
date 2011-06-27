de85f14e262a458742faa81d68a2766c03da4383
Dave Newton <davelnewton@gmail.com>
2011-06-27 00:15:29 -0400

    Added gravatar_image_tag gem, created gravatar helper, updated user show/spec.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,13 +9,24 @@ describe UsersController do
         end
 
         it "should be successful and have correct title" do
-            get 'new'
+            get :show, :id => @user
             response.should be_success
+            response.should have_selector('title', :content => @user.name)
         end
 
         it "should show the right user" do
             get :show, :id => @user
             assigns(:user).should == @user
+        end
+
+        it "should show the user's name" do
+            get :show, :id => @user
+            response.should have_selector('h1', :content => @user.name)
+        end
+
+        it "should show the user's profile image" do
+            get :show, :id => @user
+            response.should have_selector('h1>img', :class => 'gravatar')
         end
     end
 
