c5fd3d28e9036db201f7ea610693b8ed9d96b00a
Dave Newton <davelnewton@gmail.com>
2011-06-26 23:55:20 -0400

    Added a :user factory and a user controller 'show' spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,6 +3,22 @@ require 'spec_helper'
 describe UsersController do
     render_views
 
+    describe "GET 'show'" do
+        before :each do
+            @user = Factory :user
+        end
+
+        it "should be successful and have correct title" do
+            get 'new'
+            response.should be_success
+        end
+
+        it "should show the right user" do
+            get :show, :id => @user
+            assigns(:user).should == @user
+        end
+    end
+
     describe "GET 'new'" do
         it "should be successful and have correct title" do
             get 'new'
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+    user.name "Dave Newton"
+    user.email "davelnewton@gmail.com"
+    user.password "foobar"
+    user.password_confirmation "foobar"
+end
