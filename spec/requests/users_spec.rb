09d118fdff02074e89e8166301f24580261927f0
Dave Newton <davelnewton@gmail.com>
2011-06-27 23:16:01 -0400

    sign in/out, rspecs for same.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,49 +2,78 @@ require 'spec_helper'
 
 describe "Users" do
 
-    describe "failure" do
-        it "should not make a new user" do
-            lambda do
-                visit signup_path
-                fill_in "Name", :with => ""
-                fill_in "Email", :with => ""
-                fill_in "Password", :with => ""
-                fill_in "Confirmation", :with => ""
-                click_button
+    describe "signup" do
+        describe "failure" do
+            it "should not make a new user" do
+                lambda do
+                    visit signup_path
+                    fill_in "Name", :with => ""
+                    fill_in "Email", :with => ""
+                    fill_in "Password", :with => ""
+                    fill_in "Confirmation", :with => ""
+                    click_button
 
-                response.should render_template('users/new')
-                response.should have_selector("div#error_explanation")
-            end.should_not change(User, :count)
-        end
+                    response.should render_template('users/new')
+                    response.should have_selector("div#error_explanation")
+                end.should_not change(User, :count)
+            end
 
-        it "should clear the password fields" do
-            visit signup_path
-            fill_in "Name", :with => "Example User"
-            fill_in "Email", :with => "" # Force form failure
-            fill_in "Password", :with => "foobar"
-            fill_in "Confirmation", :with => "foobar"
-            click_button
-
-            response.should render_template('users/new')
-            response.should have_selector("div#error_explanation")
-
-            field_labeled("Password").value.should == ""
-            field_labeled("Confirmation").value.should == ""
-        end
-    end
-
-    describe "success" do
-        it "should make a new user" do
-            lambda do
+            it "should clear the password fields" do
                 visit signup_path
                 fill_in "Name", :with => "Example User"
-                fill_in "Email", :with => "wtf@over.com"
+                fill_in "Email", :with => "" # Force form failure
                 fill_in "Password", :with => "foobar"
                 fill_in "Confirmation", :with => "foobar"
                 click_button
 
-                response.should render_template('users/show')
-            end.should change(User, :count).by(1)
+                response.should render_template('users/new')
+                response.should have_selector("div#error_explanation")
+
+                field_labeled("Password").value.should == ""
+                field_labeled("Confirmation").value.should == ""
+            end
+        end
+
+        describe "success" do
+            it "should make a new user" do
+                lambda do
+                    visit signup_path
+                    fill_in "Name", :with => "Example User"
+                    fill_in "Email", :with => "wtf@over.com"
+                    fill_in "Password", :with => "foobar"
+                    fill_in "Confirmation", :with => "foobar"
+                    click_button
+
+                    response.should render_template('users/show')
+                end.should change(User, :count).by(1)
+            end
+        end
+    end
+
+    describe "sign in/out" do
+        describe "failure" do
+            it "should not sign the user in" do
+                visit signin_path
+                fill_in :email,    :with => ''
+                fill_in :password, :with => ''
+                click_button
+
+                response.should have_selector('div.flash.error', :content => 'Invalid')
+            end
+        end
+
+        describe "success" do
+            it "should sign a user in, then out" do
+                user = Factory(:user)
+                visit signin_path
+                fill_in :email,    :with => user.email
+                fill_in :password, :with => user.password
+                click_button
+
+                controller.should be_signed_in
+                click_link 'Sign out'
+                controller.should_not be_signed_in
+            end
         end
     end
 
