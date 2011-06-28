ff56e9d982d8ed1f8ca92aaba100f5e5e5681fdd
Dave Newton <davelnewton@gmail.com>
2011-06-28 08:32:51 -0400

    Added sample microposts, pagination, fake data, and micropost display to user profile.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -56,32 +56,45 @@ describe UsersController do
         end
     end
 
-    describe "GET 'show'" do
+    describe "GET 'show' (users/n)" do
         before(:each) do
             @user = Factory :user
         end
 
-        it "should be successful and have correct title" do
-            get :show, :id => @user
-            response.should be_success
-            response.should have_selector('title', :content => @user.name)
+        describe "basic user profile page" do
+            it "should be successful and have correct title" do
+                get :show, :id => @user
+                response.should be_success
+                response.should have_selector('title', :content => @user.name)
+            end
+
+            it "should show the right user" do
+                get :show, :id => @user
+                assigns(:user).should == @user
+            end
+
+            it "should show the user's name" do
+                get :show, :id => @user
+                response.should have_selector('h1', :content => @user.name)
+            end
+
+            it "should show the user's profile image" do
+                get :show, :id => @user
+                response.should have_selector('h1>img', :class => 'gravatar')
+            end
         end
 
-        it "should show the right user" do
-            get :show, :id => @user
-            assigns(:user).should == @user
-        end
-
-        it "should show the user's name" do
-            get :show, :id => @user
-            response.should have_selector('h1', :content => @user.name)
-        end
-
-        it "should show the user's profile image" do
-            get :show, :id => @user
-            response.should have_selector('h1>img', :class => 'gravatar')
+        describe "user profile microposts" do
+            it "should display the user's microposts" do
+                mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+                mp2 = Factory(:micropost, :user => @user, :content => "Baz quux")
+                get :show, :id => @user
+                response.should have_selector("span.content", :content => mp1.content)
+                response.should have_selector("span.content", :content => mp2.content)
+            end
         end
     end
+
 
     describe "GET 'new'" do
         it "should be successful and have correct title" do
